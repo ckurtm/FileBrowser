6e502f29d3631a0ee7740215f0769542096142f8
ckurtm <ckurtm@gmail.com>
2016-02-12 10:38:55 +0200

    Initial commit

---
+++ app/src/main/java/com/peirr/filebrowser/FileBrowser.java
@@ -0,0 +1,13 @@
+package com.peirr.filebrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FileBrowser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_browser);
+    }
+}
